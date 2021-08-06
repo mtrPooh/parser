9c5238742e9f989a6a2ce5db632706434af5e47e
mtrPooh <it-exxpert@yandex.ru>
2021-08-07 02:27:05 +0500

    MUH parser: added items for parse

--- app/Feeds/Vendors/MUH/Parser.php
+++ app/Feeds/Vendors/MUH/Parser.php
@@ -2,16 +2,42 @@
 
 namespace App\Feeds\Vendors\MUH;
 
+use App\Feeds\Feed\FeedItem;
 use App\Feeds\Parser\HtmlParser;
+use App\Feeds\Utils\Link;
 use App\Feeds\Utils\ParserCrawler;
 use App\Helpers\StringHelper;
 
 class Parser extends HtmlParser
 {
+    private const   API_URL = 'https://www.mudhole.com/api/items?include=facets&fieldset=details&language=en&country=US&currency=USD&url=';
+    private ?string $brand  = null;
+    private ?float  $weight = null;
+    private ?array  $attrs  = null;
+    
     public function beforeParse(): void
     {
+        $this->filter( 'div.specs-content table tr' )->each( function ( ParserCrawler $c )  {
+            $name  = trim($c->filter( 'td' )->getNode( 0 )->textContent, ' : ' );
+            $value = trim($c->filter( 'td' )->getNode( 1 )->textContent, '  ' );
+            
+            if ( $name == 'Brand' ) {
+                $this->brand = $value;
+            }
+            elseif ( $name == 'Weight (oz)' ) {
+                $this->weight = $value;
+            }
+            else {
+                $this->attrs[$name] = $value;
+            }
+        });
     }
-
+    
+    public function isGroup(): bool
+    {
+        return $this->exists( 'a[data-toggle="set-option"]' );
+    }
+    
     public function getMpn(): string
     {
         return $this->getText( 'span[itemprop="sku"]' ) ?? '';
@@ -24,16 +50,84 @@ class Parser extends HtmlParser
 
     public function getCostToUs(): float
     {
+        if ( $this->isGroup() ) {
+            return 0;
+        }
         return StringHelper::getMoney( $this->getMoney( 'span[itemprop="price"]' ) );
+    }
+    
+    public function getListPrice(): ?float
+    {
+        return StringHelper::getMoney( $this->getMoney( 'span[itemprop="offers"] small.muted.crossed' ) ) ?? null;
     }
 
     public function getImages(): array
     {
-        return array_values( array_unique( $this->getSrcImages( 'li.pinterest-image a img' ) ) );
+        $images = $this->getSrcImages( '.pinterest-image a#btn-lightbox-image noscript img' ) ?:
+                  $this->getSrcImages( '.pinterest-image a#btn-lightbox-image img' );
+        return array_values( array_unique( $images ) );
     }
     
     public function getAvail(): ?int
     {
-        return self::DEFAULT_AVAIL_NUMBER;
+        $stock_status = $this->getAttr( 'meta[name="og:availability"]', 'content' );
+        return $stock_status == 'InStock' ? self::DEFAULT_AVAIL_NUMBER : 0;
+    }
+    
+    public function getCategories(): array
+    {
+        $categories = $this->getContent( '.breadcrumb li a' );
+        array_shift( $categories );
+        return $categories;
+    }
+    
+    public function getDescription(): string
+    {
+        return trim( $this->getHtml( 'div.text-cont' ) );
+    }
+    
+    public function getBrand(): ?string
+    {
+        return $this->brand ?? null;
+    }
+    
+    public function getWeight(): ?float
+    {
+        return $this->weight ?? null;
+    }
+    
+    public function getAttributes(): ?array
+    {
+        return $this->attrs ?? null;
+    }
+    
+    public function getChildProducts( FeedItem $parent_fi ): array
+    {
+        $child = [];
+    
+        $product_uri = explode( '/', trim( $parent_fi->supplier_internal_id, '/ ' ) );
+        $product_uri = end( $product_uri );
+        
+        $link = new Link( self::API_URL . $product_uri );
+
+        $products = $this->getVendor()->getDownloader()->fetch( [ $link ], true )[ 0 ];
+        $products_data = json_decode( $products[ 'data' ], true, 512, JSON_THROW_ON_ERROR );
+
+        $product_name = $products_data[ 'items' ][ 0 ][ 'storedisplayname2' ];
+        
+        foreach ( $products_data[ 'items' ][ 0 ][ 'matrixchilditems_detail' ] as $product_data ) {
+            
+            $fi = clone $parent_fi;
+            
+            $fi->setMpn( $product_data[ 'itemid' ] );
+            $fi->setProduct( $product_name );
+            $fi->setCostToUs( StringHelper::getMoney( $product_data[ 'onlinecustomerprice' ] ) );
+            $fi->setListPrice( StringHelper::getMoney( $product_data[ 'pricelevel9' ] ) );
+            $fi->setRAvail( $product_data[ 'isinstock' ] ? self::DEFAULT_AVAIL_NUMBER : 0 );
+            
+            $child[] = $fi;
+        }
+
+        return $child;
     }
 }
--- app/Feeds/Vendors/MUH/Vendor.php
+++ app/Feeds/Vendors/MUH/Vendor.php
@@ -8,13 +8,18 @@ use App\Feeds\Processor\HttpProcessor;
 class Vendor extends HttpProcessor
 {
     public const CATEGORY_LINK_CSS_SELECTORS = [ 'li.level-2 a', '.category-cell-name a', '.pagination-links li a' ];
-    public const PRODUCT_LINK_CSS_SELECTORS = [ 'tr.item-cell-line td a.', 'h2.item-cell-name a' ];
+    public const PRODUCT_LINK_CSS_SELECTORS = [ 'tr.item-cell-line td a', 'h2.item-cell-name a' ];
 
     protected array $first = [ 'https://www.mudhole.com/' ];
-    protected ?int $max_products = 1;
 
     public function isValidFeedItem( FeedItem $fi ): bool
     {
-        return !empty( $fi->getMpn() );
+        if ( $fi->isGroup() ) {
+            $fi->setChildProducts( array_values(
+                                       array_filter( $fi->getChildProducts(), static fn( FeedItem $item ) => !empty( $item->getMpn() ) && count( $item->getImages() ) && $item->getCostToUs() > 0 )
+                                   ) );
+            return count( $fi->getChildProducts() );
+        }
+        return !empty( $fi->getMpn()) && count( $fi->getImages() ) && $fi->getCostToUs() > 0;
     }
 }
