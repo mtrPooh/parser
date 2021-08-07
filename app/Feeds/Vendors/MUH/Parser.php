440d1ab48604fecac0004243b9e9364019923da7
mtrPooh <it-exxpert@yandex.ru>
2021-08-08 04:17:51 +0500

    MUH parser: added items to parse

--- app/Feeds/Vendors/MUH/Parser.php
+++ app/Feeds/Vendors/MUH/Parser.php
@@ -7,6 +7,7 @@ use App\Feeds\Parser\HtmlParser;
 use App\Feeds\Utils\Link;
 use App\Feeds\Utils\ParserCrawler;
 use App\Helpers\StringHelper;
+use App\Helpers\FeedHelper;
 
 class Parser extends HtmlParser
 {
@@ -14,23 +15,67 @@ class Parser extends HtmlParser
     private ?string $brand  = null;
     private ?float  $weight = null;
     private ?array  $attrs  = null;
+    private ?string $descr  = null;
+    private array   $files  = [];
+    private array   $videos = [];
+    private array   $dims   = [];
     
     public function beforeParse(): void
     {
+        // Attributes
         $this->filter( 'div.specs-content table tr' )->each( function ( ParserCrawler $c )  {
-            $name  = trim($c->filter( 'td' )->getNode( 0 )->textContent, ' : ' );
-            $value = trim($c->filter( 'td' )->getNode( 1 )->textContent, '  ' );
+            $name  = trim( $c->filter( 'td' )->getNode( 0 )->textContent, ' : ' );
+            $value = trim( $c->filter( 'td' )->getNode( 1 )->textContent, '  ' );
             
             if ( $name == 'Brand' ) {
                 $this->brand = $value;
             }
             elseif ( $name == 'Weight (oz)' ) {
-                $this->weight = $value;
+                $this->weight = FeedHelper::convertLbsFromOz( (float) $value );
+            }
+            elseif ( $name == 'Length' ) {
+                $this->dims[ 'z' ] = (float) ( str_replace("'", '.', trim( $value, '" ') ) );
             }
             else {
-                $this->attrs[$name] = $value;
+                $this->attrs[ $name ] = $value;
             }
         });
+    
+        // Files
+        $this->filter( 'div#Resources .text-center a[target="_blank"]' )->each( function ( ParserCrawler $c )  {
+            $name = trim( $c->text(), ' : ' );
+            $link = $c->getAttr( 'a', 'href' );
+            if ( !str_contains($link, 'http' ) ) {
+                $link = 'https://www.mudhole.com' . $link;
+            }
+            $this->files[] = [ 'name' => $name, 'link' => $link ];
+        });
+    
+        // Videos
+        $this->filter( 'div.youtubevideo' )->each( function ( ParserCrawler $c )  {
+            $yt_src = $c->getAttr( 'div', 'data-yt-src' );
+            if ( !empty( $yt_src ) ) {
+                $this->videos[] = [ 'name' => 'Video',
+                                    'video' => 'https://www.youtube.com/watch?v=' . $yt_src,
+                                    'provider' => 'YouTube' ];
+            }
+        });
+    
+        // Description
+        // html-code on site is incorrect and getHtml() return unnecessary code
+        // it need to remove from Description
+        $this->descr = $this->getHtml( 'div.text-cont' );
+        $videos = $this->getHtml( 'div#videos' );
+        $resources = $this->getHtml( 'div#Resources' );
+        $spec = $this->getHtml( 'div#spec' );
+    
+        $del[] = '<div class="tab-pane" id="videos"></div>';
+        $del[] = '<div class="tab-pane" id="Resources"></div>';
+        $del[] = '<div class="tab-pane" id="spec"></div>';
+    
+        $this->descr = str_ireplace( [ $videos, $resources, $spec ], '', $this->descr );
+        $this->descr = str_ireplace( $del, '', $this->descr );
+        $this->descr = trim( preg_replace( '%<table.*?</table>%uis', '', $this->descr ) );
     }
     
     public function isGroup(): bool
@@ -53,25 +98,35 @@ class Parser extends HtmlParser
         if ( $this->isGroup() ) {
             return 0;
         }
-        return StringHelper::getMoney( $this->getMoney( 'span[itemprop="price"]' ) );
+        return $this->getMoney( 'span[itemprop="price"]' );
     }
     
     public function getListPrice(): ?float
     {
-        return StringHelper::getMoney( $this->getMoney( 'span[itemprop="offers"] small.muted.crossed' ) ) ?? null;
+        return $this->getMoney( 'span[itemprop="offers"] small.muted.crossed' );
     }
 
     public function getImages(): array
     {
         $images = $this->getSrcImages( '.pinterest-image a#btn-lightbox-image noscript img' ) ?:
                   $this->getSrcImages( '.pinterest-image a#btn-lightbox-image img' );
+        
+        for ( $i = 0; $i < count( $images ); $i++ ) {
+            $filename = pathinfo( $images[ $i ] );
+            $filename = $filename[ 'basename' ];
+            if ( str_contains( $filename, '?' ) ) {
+                $new_filename = substr( $filename, 0, strpos( $filename, '?' ) );
+                $images[ $i ] = str_replace( $filename, $new_filename, $images[ $i ] );
+            }
+        }
+        
         return array_values( array_unique( $images ) );
     }
     
     public function getAvail(): ?int
     {
         $stock_status = $this->getAttr( 'meta[name="og:availability"]', 'content' );
-        return $stock_status == 'InStock' ? self::DEFAULT_AVAIL_NUMBER : 0;
+        return $stock_status === 'InStock' ? self::DEFAULT_AVAIL_NUMBER : 0;
     }
     
     public function getCategories(): array
@@ -83,7 +138,7 @@ class Parser extends HtmlParser
     
     public function getDescription(): string
     {
-        return trim( $this->getHtml( 'div.text-cont' ) );
+        return $this->descr;
     }
     
     public function getBrand(): ?string
@@ -101,6 +156,21 @@ class Parser extends HtmlParser
         return $this->attrs ?? null;
     }
     
+    public function getProductFiles(): array
+    {
+        return $this->files;
+    }
+    
+    public function getVideos(): array
+    {
+        return $this->videos;
+    }
+    
+    public function getDimZ(): ?float
+    {
+        return $this->dims[ 'z' ] ?? null;
+    }
+    
     public function getChildProducts( FeedItem $parent_fi ): array
     {
         $child = [];
@@ -113,16 +183,31 @@ class Parser extends HtmlParser
         $products = $this->getVendor()->getDownloader()->fetch( [ $link ], true )[ 0 ];
         $products_data = json_decode( $products[ 'data' ], true, 512, JSON_THROW_ON_ERROR );
         
-        if ( empty( $products_data[ 'items' ][ 0 ][ 'matrixchilditems_detail' ] ) ) return [];
-
-        $product_name = $products_data[ 'items' ][ 0 ][ 'storedisplayname2' ];
+        if ( empty( $products_data[ 'items' ][ 0 ][ 'matrixchilditems_detail' ] ) ) {
+            return [];
+        }
+    
+        $option_labels = [];
+        foreach ( $products_data[ 'items' ][ 0 ][ 'itemoptions_detail' ][ 'fields' ] as $option ) {
+            if ( empty( $option[ 'ismandatory' ] ) || empty( $option[ 'sourcefrom' ] ) || empty( $option[ 'label' ] ) ) {
+                continue;
+            }
+            $option_labels[ $option[ 'sourcefrom' ] ] = ucfirst( $option[ 'label' ] );
+        }
         
         foreach ( $products_data[ 'items' ][ 0 ][ 'matrixchilditems_detail' ] as $product_data ) {
             
             $fi = clone $parent_fi;
+    
+            $product_name = [];
+            foreach ( $option_labels as $key => $value ) {
+                if ( !empty( $product_data[ $key ] ) ) {
+                    $product_name[] = $value . ': ' . $product_data[ $key ];
+                }
+            }
             
             $fi->setMpn( $product_data[ 'itemid' ] );
-            $fi->setProduct( $product_name );
+            $fi->setProduct( implode(', ', $product_name) );
             $fi->setCostToUs( StringHelper::getMoney( $product_data[ 'onlinecustomerprice' ] ) );
             $fi->setListPrice( StringHelper::getMoney( $product_data[ 'pricelevel9' ] ) );
             $fi->setRAvail( $product_data[ 'isinstock' ] ? self::DEFAULT_AVAIL_NUMBER : 0 );
--- app/Feeds/Vendors/MUH/Vendor.php
+++ app/Feeds/Vendors/MUH/Vendor.php
@@ -9,15 +9,15 @@ class Vendor extends HttpProcessor
 {
     public const CATEGORY_LINK_CSS_SELECTORS = [ 'li.level-2 a', '.category-cell-name a', '.pagination-links li a' ];
     public const PRODUCT_LINK_CSS_SELECTORS = [ 'tr.item-cell-line td a', 'h2.item-cell-name a' ];
-
+    
     protected array $first = [ 'https://www.mudhole.com/' ];
-
+    
     public function isValidFeedItem( FeedItem $fi ): bool
     {
         if ( $fi->isGroup() ) {
             $fi->setChildProducts( array_values(
-                                       array_filter( $fi->getChildProducts(), static fn( FeedItem $item ) => !empty( $item->getMpn() ) && count( $item->getImages() ) && $item->getCostToUs() > 0 )
-                                   ) );
+                 array_filter( $fi->getChildProducts(), static fn( FeedItem $item ) => !empty( $item->getMpn() ) && count( $item->getImages() ) && $item->getCostToUs() > 0 )
+            ) );
             return count( $fi->getChildProducts() );
         }
         return !empty( $fi->getMpn()) && count( $fi->getImages() ) && $fi->getCostToUs() > 0;
