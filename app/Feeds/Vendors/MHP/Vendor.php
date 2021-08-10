722c932fca46c1fa17cf0ed2c14c540cbb44fb07
mtrPooh <it-exxpert@yandex.ru>
2021-08-11 00:22:40 +0500

    MHP parser: fix

--- app/Feeds/Vendors/MHP/Vendor.php
+++ app/Feeds/Vendors/MHP/Vendor.php
@@ -14,11 +14,6 @@ class Vendor extends HttpProcessor
 
     public function isValidFeedItem( FeedItem $fi ): bool
     {
-        if ( !$fi->getMpn() ) echo 'Mpn: ' . $fi->getSupplierInternalId(), PHP_EOL;
-        if ( !$fi->getCostToUs() ) echo 'CostToUs: ' . $fi->getSupplierInternalId(), PHP_EOL;
-        if ( !$fi->getImages() ) echo 'Images: ' . $fi->getSupplierInternalId(), PHP_EOL;
-        if ( !$fi->fulldescr ) echo 'Descr: ' . $fi->getSupplierInternalId(), PHP_EOL;
-
         return !empty( $fi->getMpn() ) && count( $fi->getImages() ) && $fi->getCostToUs() > 0;
     }
 }
