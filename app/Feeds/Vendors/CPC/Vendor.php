67ad7b7ae5a78229f9d4a3f364b66e36c1002652
mtrPooh <it-exxpert@yandex.ru>
2021-08-11 14:44:07 +0500

    CPC parser: fix

--- app/Feeds/Vendors/CPC/Vendor.php
+++ app/Feeds/Vendors/CPC/Vendor.php
@@ -11,6 +11,9 @@ class Vendor extends HttpProcessor
     public const CATEGORY_LINK_CSS_SELECTORS = [ 'ul.sub-menu a', 'li.product-category a', 'a.uabb-new-ib-link', 'a.page-numbers' ];
     public const PRODUCT_LINK_CSS_SELECTORS = [ 'li.product.type-product a' ];
 
+    protected const STATIC_USER_AGENT = true;
+    protected array $headers = [ 'Accept: *', 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0' ];
+
     protected array $first = [ 'https://carolinapetcompany.com/' ];
 
     public function filterProductLinks( Link $link ): bool
